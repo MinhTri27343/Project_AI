a7a5456cc271162000df8e5b74ee70d1685c3c91
MinhTri27343 <158994147+MinhTri27343@users.noreply.github.com>
2025-03-12 23:05:59 +0700

    Fix ghost position level 1 -> 4

--- Level/Level1.py
+++ Level/Level1.py
@@ -1,21 +1,23 @@
 from utils import *
 from Object.Game import Game
 from Test.TestMenu import *
+from board import boards
+import utils
 class Level1: 
     def __init__(self, setUp, setMoving):
         self.screen = setUp.screen
         self.ghosts = [setUp.ghosts[0]]
         self.algorithms = [setUp.algorithms[0]]
         self.moving = setMoving
-        self.game = Game(setUp, setMoving, self.ghosts, self.algorithms, 5)
+        self.game = Game(setUp, setMoving, self.ghosts, self.algorithms, 0)
     def execute(self):
             #=====================================
             menu_test = TestMenu(self.screen)
             menu_test.execute()
             ind_test = menu_test.ind_test
             pos_ghost, pos_player = FactoryTest.getProperties()["Test" + str(ind_test + 1)].getTest()
-            self.game.ghosts[0].x_pos, self.game.ghosts[0].y_pos = pos_ghost
-            self.game.player.x, self.game.player.y = pos_player
+            self.ghosts[0].setNewPosition(pos_ghost)
+            self.game.player.setNewPosition(pos_player)
             #=====================================
             self.game.run()
             
